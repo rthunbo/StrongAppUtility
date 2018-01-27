925f940e0d643c57b2f6c6853ad487c4d8b54dc4
Rolf Thunbo <rthunbo@gmail.com>
2018-01-27 17:10:39 +0100

    Move volume calculation to VolumeCalculator.

--- StrongAppUtility/Program.cs
+++ StrongAppUtility/Program.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using StrongAppData.Model;
-using StrongAppData.Repositories;
-using StrongAppData.Extensions;
-
-namespace StrongAppData
+﻿namespace StrongAppData
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using StrongAppData.Model;
+    using StrongAppData.Repositories;
+    using StrongAppData.Extensions;
+    using StrongAppData.Services;
+
     static class Program
     {
-        static ExerciseRepository exerciseRepository = new ExerciseRepository();
         static WorkoutRepository workoutRepository = new WorkoutRepository();
-        static MeasurementRepository measurementRepository = new MeasurementRepository();
+        static VolumeCalculator volumeCalculator = new VolumeCalculator(new ExerciseRepository(), new MeasurementRepository());
 
         static void Main(string[] args)
         {
@@ -32,7 +32,7 @@ namespace StrongAppData
                 Console.WriteLine($"Week: {weeklyWorkouts.YearAndWeek}");
 
                 var totalWeeklyVolumePerBodyPart = weeklyWorkouts.Workouts
-                    .SelectMany(w => GetWorkoutVolume(w))
+                    .SelectMany(w => volumeCalculator.GetWorkoutVolume(w))
                     .GroupBy(x => x.BodyPart)
                     .Select(g => new Volume { BodyPart = g.Key, Value = g.Sum(x => x.Value) })
                     .OrderByDescending(x => x.Value);
@@ -47,42 +47,5 @@ namespace StrongAppData
             }
         }
 
-        private static IEnumerable<Volume> GetWorkoutVolume(Workout w)
-        {
-            var exerciseVolumes = w.Work
-                .GroupBy(x => x.ExerciseName)
-                .Select(g => new 
-                {
-                    BodyPart = exerciseRepository.GetExercise(g.Key).BodyPart,
-                    Volume = g.Sum(x => GetWorkItemVolume(w, x))
-                })
-                .GroupBy(x => x.BodyPart)
-                .Select(g => new Volume
-                {
-                    BodyPart = g.Key,
-                    Value = g.Sum(x => x.Volume)
-                });
-
-            return exerciseVolumes;
-        }
-
-        private static float GetWorkItemVolume(Workout workout, Workout.WorkItem workItem)
-        {
-            float kg = workItem.Kg;
-            var exercise = exerciseRepository.GetExercise(workItem.ExerciseName);
-            if (exercise.Category == Category.WeightedBodyweight)
-            {
-                var measurement = measurementRepository.GetMeasurement(workout.Date);
-                if (measurement != null)
-                {
-                    kg += measurement.Weight;
-                }
-            }
-            else if (exercise.Category == Category.Dumbbell)
-            {
-                kg *= 2;
-            }
-            return kg * workItem.Reps;
-        }
     }
 }
---
+++ StrongAppUtility/Services/VolumeCalculator.cs
@@ -0,0 +1,63 @@
+﻿namespace StrongAppData.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using StrongAppData.Model;
+    using StrongAppData.Repositories;
+
+    public class VolumeCalculator
+    {
+        private readonly ExerciseRepository exerciseRepository;
+        private readonly MeasurementRepository measurementRepository;
+
+        public VolumeCalculator(ExerciseRepository exerciseRepository, MeasurementRepository measurementRepository)
+        {
+            this.exerciseRepository = exerciseRepository;
+            this.measurementRepository = measurementRepository;
+        }
+
+        public IEnumerable<Volume> GetWorkoutVolume(Workout w)
+        {
+            var exerciseVolumes = w.Work
+                .GroupBy(x => x.ExerciseName)
+                .Select(g => new
+                {
+                    BodyPart = exerciseRepository.GetExercise(g.Key).BodyPart,
+                    Volume = g.Sum(x => GetWorkItemVolume(w, x))
+                })
+                .GroupBy(x => x.BodyPart)
+                .Select(g => new Volume
+                {
+                    BodyPart = g.Key,
+                    Value = g.Sum(x => x.Volume)
+                });
+
+            return exerciseVolumes;
+        }
+
+        private float GetWorkItemVolume(Workout workout, Workout.WorkItem workItem)
+        {
+            float kg = workItem.Kg;
+            var exercise = exerciseRepository.GetExercise(workItem.ExerciseName);
+            if (exercise.Category == Category.WeightedBodyweight)
+            {
+                var measurement = measurementRepository.GetMeasurement(workout.Date);
+                if (measurement != null)
+                {
+                    kg += measurement.Weight;
+                }
+            }
+            else if (exercise.Category == Category.AssistedBodyweight)
+            {
+                kg *= -1;
+                var measurement = measurementRepository.GetMeasurement(workout.Date);
+                if (measurement != null)
+                {
+                    kg += measurement.Weight;
+                }
+            }
+            return kg * workItem.Reps;
+        }
+
+    }
+}
